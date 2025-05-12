6d83a826eeb9891cf28e996363a009e314f888f4
SachinKumarMishra87 <sachinmishra8707@gmail.com>
2025-05-12 21:00:17 +0530

    auht

--- server_socket/controllers/authController.js
+++ server_socket/controllers/authController.js
@@ -42,8 +42,8 @@ export const register = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -110,8 +110,8 @@ export const login = async (req, res) => {
 
         return res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            samesite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         }).json({
             success: true,
@@ -133,8 +133,8 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie("token", "", {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            samesite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            secure: true,
+            sameSite: "None",
         })
 
         return res.json({
